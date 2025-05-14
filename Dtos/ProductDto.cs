d63ea2f9472c33f3e4a5517d5e8e9e4ca4e57463
Thomas Alfonso <thomalfonso@gmail.com>
2025-05-13 20:51:03 -0400

    fix: fixed name validation

--- Dtos/ProductDto.cs
+++ Dtos/ProductDto.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations;
 public class ProductCreateDto
 {
     [Required(ErrorMessage = "Name is required")]
-    [RegularExpression(@"\S+", ErrorMessage = "Name cannot be just spaces")]
     public string Name { get; set; } = string.Empty;
 
     [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
